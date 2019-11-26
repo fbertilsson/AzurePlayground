9d196e1d9fc9972d97a2059e529724d32adf074f
Fredrik Bertilsson <fredrik@helenaofredrik.se>
2019-11-26 21:28:04 +0100

    Fixed terraform reference syntax

--- ServiceBusReader/Terraform.tf
+++ ServiceBusReader/Terraform.tf
@@ -5,8 +5,8 @@
 
 resource "azurerm_servicebus_namespace" "sb" {
   name                = "fbservicebusreader"
-  location            = "${azurerm_resource_group.rg.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
 
   tags = {
@@ -16,16 +16,16 @@ resource "azurerm_servicebus_namespace" "sb" {
 
 resource "azurerm_servicebus_topic" "topic1" {
   name                = "topic1"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.sb.name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  namespace_name      = azurerm_servicebus_namespace.sb.name
 
   enable_partitioning = true
 }
 
 resource "azurerm_servicebus_subscription" "sub1" {
   name                = "sub1"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.sb.name}"
-  topic_name          = "${azurerm_servicebus_topic.topic1.name}"
-  max_delivery_count  = 5
+  resource_group_name = azurerm_resource_group.rg.name
+  namespace_name      = azurerm_servicebus_namespace.sb.name
+  topic_name          = azurerm_servicebus_topic.topic1.name
+  max_delivery_count  = 6
 }
